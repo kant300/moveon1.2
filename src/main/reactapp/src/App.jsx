80227665df9fbe9756ad4cd87b826d2cd5dea6ab
IceP0tato <rlawodud0313@gmail.com>
2025-10-21 14:54:08 +0900

    251021_지하철 코드 react에 적용, css 적용 테스트

--- src/main/reactapp/src/App.jsx
+++ src/main/reactapp/src/App.jsx
@@ -6,6 +6,7 @@ import Menu from "./components/Menu";
 import Setting from "./components/Setting";
 import MyPage from "./components/MyPage";
 import Station from './components/transport/Station.jsx';
+import Test from './components/living/Test.jsx';
 
 // 현재 홈, 마이페이지, 전체메뉴, 설정까지 프로토타입 제작 완료
 export default function App() {
@@ -19,6 +20,8 @@ export default function App() {
           <Route path='/setting' element={<Setting />} />
 
           {/* living */}
+
+          <Route path='/trashInfo' element={<Test />} />
 
           <Route path='/subway' element={<Station />} />
         </Routes>
