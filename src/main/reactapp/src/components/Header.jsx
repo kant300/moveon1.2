c540a473dc348c0b030006a38f0530980328b41d
MSI <kgs2072@naver.com>
2025-10-28 00:43:30 +0900

    강사피드백.
    1. Header.jsx : 로그인과 비로그인시 헤더 메뉴 다르게
    2. Setting.jsx : 로그아웃 구현
    3. Index.jsx , Index.css , footer.jsx , footer.css

--- src/main/reactapp/src/components/Header.jsx
+++ src/main/reactapp/src/components/Header.jsx
@@ -11,20 +11,34 @@ import notice from '../assets/images/icons/notification_sound_24dp_1F1F1F_FILL0_
 
 // Header
 export default function Header() {
-    // 로그인된 멤버 정보 저장
-    const [ member, setMember ]= useState(null);
+
+    // 1. 로그인된 유저 정보 저장
+    const [ member , setMember ] = useState( null );
+    // 2. 최초로 컴포넌트 실행시 유저 정보 요청하기
+    const getMyInfo = async()=>{
+        try{
+            const url = "http://localhost:8080/api/member/info"
+            const res = await axios.get( url , { withCredentials : true } );
+            setMember( res.data ); // 반환된 유저 정보를 저장
+        }catch( err ){ setMember(null); } // 오류시 null
+    }
+    useEffect( () => { getMyInfo(); } , [] );
+    
     const navigate = useNavigate();
 
        return (<>
         <div id="header">
             <Link to='/'>mOveOn</Link>
             <div id= "headerRight">
-                <Link to='/notification'> {/*알림버튼*/}
-                    <img src={notice} id="headerBtn" alt="알림" />
-                </Link>
-                <Link to='/login'>{/*로그인버튼*/}
-                    <img src={login} id="headerBtn" />
-                </Link>
+                { member ? <>                 
+                        <Link to='/notification'> {/*알림버튼*/}
+                            <img src={notice} id="headerBtn" alt="알림" />
+                        </Link> </> 
+                        : <>                 
+                        <Link to='/login'>{/*로그인버튼*/}
+                            <img src={login} id="headerBtn" />
+                        </Link> </>
+                }
             </div>
         </div>
     </>)
